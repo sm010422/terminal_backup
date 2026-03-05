9e444cc4a51f709010e9b86fce034134222c991d
Park Sang Min <sm0104224@gmail.com>
2026-03-06 00:28:06 +0900

    refactor(.hammerspoon): delete alert.show and change delay time 0.1 to 0.05

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,17 +1,14 @@
 -- 영어 입력기 ID 설정 (가급적 상수로 관리)
 local ENGLISH_SOURCE = "com.apple.keylayout.ABC"
 
--- 입력 소스를 변경하는 함수 (안정성 강화)
 local function switchToEnglish()
-    -- 현재 소스를 확인하여 이미 영어라면 중복 실행 방지
     if hs.keycodes.currentSourceID() ~= ENGLISH_SOURCE then
-        -- 시스템 부하를 줄이기 위해 아주 짧은 지연 후 실행 (안정성 핵심)
-        hs.timer.doAfter(0.1, function()
+        -- 0.05초(50ms) 지연: 딜레이는 줄이고 안정성을 확보한 수치
+        hs.timer.doAfter(0.05, function()
             hs.keycodes.currentSourceID(ENGLISH_SOURCE)
         end)
     end
 end
-
 -- 앱 변경 감지 워처
 -- 전역 변수로 선언하여 가비지 컬렉션(GC)에 의해 해제되는 것을 방지
 appWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
@@ -26,6 +23,3 @@ appWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
 end)
 
 appWatcher:start()
-
--- 설정 재발동 알림 (선택 사항)
-hs.alert.show("Hammerspoon Config Loaded")
